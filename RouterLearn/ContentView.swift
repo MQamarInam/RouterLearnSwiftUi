9c6fa0d950ccfe4761d05316d3c46c0e7732fe85
MQamarInam <qamarinam028@gmail.com>
2026-04-16 10:39:30 +0500

    Initial Commit

---
+++ RouterLearn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RouterLearn
+//
+//  Created by Muhammad Qamar on 16/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RouterLearn/RouterLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterLearnApp.swift
+//  RouterLearn
+//
+//  Created by Muhammad Qamar on 16/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct RouterLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
